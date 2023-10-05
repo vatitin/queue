9e5f27ff5ddb3b17e6fff9abb7d7696802cd4d5f
Valentin Sackmann <sackmannva@gmail.com>
2023-10-05 18:25:26 +0200

    remove comment

--- server/routes/TherapistUsers.js
+++ server/routes/TherapistUsers.js
@@ -44,43 +44,4 @@ router.post("/login", async (req, res) => {
     })
 })
 
-/*
-router.get("/byId/:therapistId", async (req, res) => {
-  try {
-    const id = req.params.therapistId;
-    const therapist = await Therapist.findByPk(id);
-    const patients = await therapist.getPatients();
-    res.json(patients);
-  }
-  catch (error) {
-    console.error('Error finding the Therapist or Patients of the Therapist:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-})
-
-router.post("/", async (req, res) => {
-    const therapist = req.body;
-    await Therapist.create(therapist)
-    res.json(therapist);
-})
-
-router.post("/addNewPatient/:therapistId", async (req, res) => {
-    const { therapistId } = req.params;
-    const patientData = req.body;
-  
-    try {
-      const therapist = await Therapist.findByPk(therapistId);
-      const patient = await Patient.create(patientData)
-      const numberOfPatients = await therapist.countPatients();
-
-      therapist.addPatient(patient, {through: {subscriptionInWaitingList: numberOfPatients}});
-  
-      res.json(patient);
-    } catch (error) {
-      console.error('Error adding new patient:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
-*/
-
 module.exports = router;
