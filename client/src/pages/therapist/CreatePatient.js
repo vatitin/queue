a256851b96fca6a488a975b09c35bf648affca9a
Valentin Sackmann <sackmannva@gmail.com>
2023-11-22 18:38:00 +0100

    style CreatePatient component

--- client/src/pages/therapist/CreatePatient.js
+++ client/src/pages/therapist/CreatePatient.js
@@ -1,69 +1,84 @@
-import React, { useEffect, useContext } from 'react';
-import {Formik, Form, Field, ErrorMessage} from 'formik';
-import * as Yup from 'yup';
+import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
-import { useNavigate, useParams } from "react-router-dom";
-import { AuthContext } from "../../helpers/AuthContext"
-import { addPatientWithStatus } from "../../endpoints"
+import { useNavigate, useParams } from 'react-router-dom';
+import { AuthContext } from '../../helpers/AuthContext';
+import { addPatientWithStatus } from '../../endpoints';
 
 function CreatePatient() {
-    const {patientStatus} = useParams();
-    const navigate = useNavigate();
-    const { authState } = useContext(AuthContext);
+  const { patientStatus } = useParams();
+  const navigate = useNavigate();
+  const { authState } = useContext(AuthContext);
 
-    const config = {
-        headers: {
-          "Content-Type": "application/json"
-          },
-          withCredentials: true
-        }
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    withCredentials: true,
+  };
 
-    const initialValues = {
-        firstName: '',
-        lastName: '',
-        email: ''
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    gender: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(addPatientWithStatus(patientStatus), formData, config);
+      navigate(`/myPatients/${patientStatus}`);
+    } catch (error) {
+      // Handle error, e.g., show a message to the user
+      console.error('Error creating patient:', error);
     }
+  };
 
-    const validationSchema = Yup.object().shape({
-        firstName: Yup.string().required('Bitte geben Sie einen Vorname ein'),
-        lastName: Yup.string().required('Bitte geben Sie einen Nachname ein'),
-        email: Yup.string()
-        .email('Bitte geben Sie eine gültige E-Mail-Adresse ein')
-        .required('Bitte geben Sie eine E-Mail-Adresse ein')    });
-
-    const onSubmit = async (data) => {
-        await axios.post(addPatientWithStatus(patientStatus), data, config)
-        navigate(`/myPatients/${patientStatus}`)
+  useEffect(() => {
+    if (!authState.status) {
+      return navigate('/loginTherapist');
     }
+  }, [authState.status, navigate]);
 
-    useEffect(() => {
-        if (!authState.status) {
-          return navigate("/loginTherapist")
-        }
-    }, [authState.status, navigate]);
-
-    return (
-        <div className="CreatePatientPage">
-            <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
-                <Form>
-                    <label>Vorname:</label>
-                    <ErrorMessage name="firstName" component="div" className="error"/>
-                    <Field id="inputPatient" name="firstName" placeholder="Max"/>
-                    
-                    <label>Nachname:</label>
-                    <ErrorMessage name="lastName" component="div" className="error"/>
-                    <Field id="inputPatient" name="lastName" placeholder="Mustermann"/>
-                    
-                    <label>Email:</label>
-                    <ErrorMessage name="email" component="div" className="error"/>
-                    <Field id="inputPatient" name="email" placeholder="meine@email.com"/>
-                    
-                    <button type="submit">Hinzufügen</button>
-
-                </Form>
-            </Formik>
+  return (
+    <div className="container">
+      <form onSubmit={onSubmit}>
+        <div className="mb-3">
+            <label htmlFor="firstName" className="form-label">Vorname</label>
+            <input className="form-control" id="firstName" name="firstName" placeholder="Max" value={formData.firstName} onChange={handleChange}/>
         </div>
-    );
+
+        <div className="mb-3"> 
+            <label htmlFor="lastName" className="form-label">Nachname</label>
+            <input className="form-control" id="lastName" name="lastName" placeholder="Mustermann" value={formData.lastName} onChange={handleChange}/>
+        </div>
+
+        <div className="mb-3">
+            <label htmlFor="email">Email</label>
+            <input className="form-control" id="email" name="email" placeholder="meine@email.com" value={formData.email} onChange={handleChange}/>
+        </div>
+
+        <div className="mb-3">
+          <label className="d-block">Geschlecht:</label>
+
+          <input type="radio" className="btn-check" id="genderM" name="gender" value="M" autoComplete="off" onChange={handleChange}/>
+          <label className="btn btn-outline-secondary" htmlFor="genderM">Männlich</label>
+
+          <input type="radio" className="btn-check" id="genderW" name="gender" value="W" autoComplete="off" onChange={handleChange}/>
+          <label className="btn btn-outline-secondary" htmlFor="genderW">Weiblich</label>
+
+          <input type="radio" className="btn-check" id="genderD" name="gender" value="D" autoComplete="off" onChange={handleChange}/>
+          <label className="btn btn-outline-secondary" htmlFor="genderD">Divers</label>
+        </div>
+
+        <button type="submit">Hinzufügen</button>
+      </form>
+    </div>
+  );
 }
 
 export default CreatePatient;
