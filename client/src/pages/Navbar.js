af1383e2e10087681fc1aa3a012bc6e28709cd7d
Valentin Sackmann <sackmannva@gmail.com>
2023-11-23 20:58:12 +0100

    adjust navbar styling

--- client/src/pages/Navbar.js
+++ client/src/pages/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
     };
 
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
       <div className="container-fluid">
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0" >
