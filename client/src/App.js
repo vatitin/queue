6f1b5b8a66eac7b77c65517880932f9732e616e5
Valentin Sackmann <sackmannva@gmail.com>
2023-12-10 19:09:39 +0100

    add supertokens ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 //import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
+import * as reactRouterDom from "react-router-dom";
 import { Home } from "./pages/Home";
 import CreatePatient from "./pages/therapist/CreatePatient";
 import { Patient } from "./pages/therapist/Patient";
@@ -11,6 +12,26 @@ import { Navbar } from "./pages/Navbar";
 import { Profile } from './pages/therapist/Profile'
 import { PageNotFound } from "./pages/PageNotFound";
 import { AuthContextProvider } from "./context/AuthContext"
+import SuperTokens, { SuperTokensWrapper } from "supertokens-auth-react";
+import EmailPassword from "supertokens-auth-react/recipe/emailpassword";
+import Session from "supertokens-auth-react/recipe/session";
+import { getSuperTokensRoutesForReactRouterDom } from "supertokens-auth-react/ui";
+import { EmailPasswordPreBuiltUI } from "supertokens-auth-react/recipe/emailpassword/prebuiltui";
+
+SuperTokens.init({
+    appInfo: {
+        // learn more about this on https://supertokens.com/docs/emailpassword/appinfo
+        appName: "Queue app",
+        apiDomain: "http://localhost:3000",
+        websiteDomain: "http://localhost:3001",
+        apiBasePath: "/auth",
+        websiteBasePath: "/auth",
+    },
+    recipeList: [
+        EmailPassword.init(),
+        Session.init()
+    ]
+});
 
 function App() {
 
@@ -18,10 +39,12 @@ function App() {
   return (
     <div className="App">
       <AuthContextProvider>
+      <SuperTokensWrapper>
         <Router>
           <Navbar/>
           <Routes>
             <Route path="/" element={<Home/>} />
+            {getSuperTokensRoutesForReactRouterDom(reactRouterDom, [EmailPasswordPreBuiltUI])}
             <Route path="/loginTherapist" element={<LoginTherapist/>} />
             <Route path="/registerTherapist" element={<RegisterTherapist/>} />
             <Route path="/addNewPatient/:patientStatus" element={<CreatePatient/>} />
@@ -31,6 +54,7 @@ function App() {
             <Route path="*" element={<PageNotFound/>} />
           </Routes>
         </Router>
+      </SuperTokensWrapper>
       </AuthContextProvider>
     </div>
   );
