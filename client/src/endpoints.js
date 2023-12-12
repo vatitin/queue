bed58652319220d822f815c49c9d033fd645ceed
Valentin Sackmann <sackmannva@gmail.com>
2023-12-12 21:23:06 +0100

    adjust endpoints

--- client/src/endpoints.js
+++ client/src/endpoints.js
@@ -3,12 +3,8 @@ const THERAPIST_PATIENTS_URL = `${BASE_URL}/therapist/patients`
 
 export const therapistProfile = `${BASE_URL}/therapist/myProfile`
 
-export const registerTherapist = `${BASE_URL}/therapistAuth/register`
-export const logoutTherapist = `${BASE_URL}/therapistAuth/logout`
-export const loginTherapist = `${BASE_URL}/therapistAuth/login`
-
 export const patientById = (id) => `${THERAPIST_PATIENTS_URL}/byId/${id}`
 export const patientsWithStatus = (status) => `${THERAPIST_PATIENTS_URL}/${status}` 
 export const deletePatientWithId = (id) => `${THERAPIST_PATIENTS_URL}/deletePatient/${id}`
 export const patientWithId = (id) => `${THERAPIST_PATIENTS_URL}/${id}`
-export const addPatientWithStatus = (status) => `${THERAPIST_PATIENTS_URL}/addNewPatient/${status}`
+export const addPatientWithStatus = (status) => `${BASE_URL}/therapist/createPatient/${status}`
